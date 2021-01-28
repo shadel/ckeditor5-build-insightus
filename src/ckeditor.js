4c4b0bc46cade195036b10d4a123f161eb373617
trieu.vo <trieugia008@gmail.com>
2021-01-28 20:43:47 +0700

    add default mention config

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -106,5 +106,15 @@ ClassicEditor.defaultConfig = {
 		]
 	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
-	language: 'en'
+	language: 'en',
+
+	mention: {
+		feeds: [
+			{
+				marker: '@',
+				feed: [ '@Barney', '@Lily', '@Marshall', '@Robin', '@Ted' ],
+				minimumCharacters: 1
+			}
+		]
+	}
 };
